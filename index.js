0e47fce8d1568f92ae31e0eeaf47b531c384c51c
Gaurav <katariagaurav686@gmail.com>
2025-03-18 18:38:13 +0530

    added food item

--- index.js
+++ index.js
@@ -5,13 +5,45 @@ document.addEventListener('DOMContentLoaded',()=>{
     const cellSize = 20;
     let score =0;
     let gameStarted = false;
-    let food = {x:200 , y:300};
+    let food = {x:400 , y:300};
     let snake = [{x:380,y:200},{x:360,y:200},{x:340,y:200}];
     let dx=cellSize;
     let dy=0;
 
+    function drawScoreBoard(){
+        const scoreBoard=document.getElementById('score-board');
+        scoreBoard.textContent=`Score: ${score}`;
+    }
 
-    function startGame(){
+    function drawDiv(x,y,className){
+        const div = document.createElement('div');
+        div.style.position='absolute';
+        div.classList.add(className);
+        div.style.top=`${y}px`;
+        div.style.left=`${x}px`;
+        return div;
+    }
+
+    function drawFoodAndSnake(){
+        gameArena.innerHTML='';
+        const foodElement = drawDiv(food.x ,food.y,'food');
+        gameArena.appendChild(foodElement);
+    }
+
+    function gameLoop(){
+        setInterval(() => {
+            drawScoreBoard();
+            drawFoodAndSnake();
+        }, 1000);
+    }
+
+    function runGame(){
+        gameStarted=true;
+
+        gameLoop();
+    }
+
+    function initiateGame(){
         const scoreBoard = document.createElement('div');
         scoreBoard.id ='score-board';
         
@@ -21,9 +53,14 @@ document.addEventListener('DOMContentLoaded',()=>{
         startButton.textContent='Start Game';
         startButton.classList.add('start-button'); 
         document.body.appendChild(startButton);
+
+        startButton.addEventListener('click',()=>{
+            startButton.style.display='none';
+            runGame();
+        })
     }
 
 
-    startGame();
+    initiateGame();
 
 })
